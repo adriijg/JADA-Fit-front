b1eb51a8fc213b76e753a99e9ee5ad6e4626c21f
DavidHoyas <thetxaby@gmail.com>
2026-05-04 17:26:57 +0200

    feat: add ApiConfig class to manage environment-specific base URLs

--- lib/core/config/api_config.dart
+++ lib/core/config/api_config.dart
@@ -1,7 +1,7 @@
 class ApiConfig {
   // Emulador Android
-  // static const String baseUrl = 'http://10.0.2.2:8080/api';
+  static const String baseUrl = 'http://10.0.2.2:8080/api';
 
   // Móvil físico en la misma WiFi
-  static const String baseUrl = 'http://192.168.1.50:8080/api';
+  //static const String baseUrl = 'http://192.168.1.50:8080/api';
 }
